be1f89fe6e603893f2370c1e04d29b07bd88de5d
katyjane8 <katy.jane8@gmail.com>
2018-01-05 09:41:43 -0700

    trying to get this many to many

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
     resources :machines, only: [:index]
   end
 
-  resources :machines, only: [:show]
+  resources :machines do
+    resources :snacks, only: [:show]
+  end
+
   resources :snacks, only: [:show]
 end
--- spec/features/user_can_see_all_snacks_spec.rb
+++ spec/features/user_can_see_all_snacks_spec.rb
@@ -9,7 +9,18 @@ describe "User can see all snacks" do
 
     visit snack_path(snack)
 
-    expect(page).to have_content(machine_snacks.name)
-    expect(page).to have_content(machine_snacks.price)
+    expect(page).to have_content(snack.name)
+    expect(page).to have_content(snack.price)
+  end
+
+  it "sees all snacks that are in that machine" do
+    snack = Snack.create(name: "White Castle Burger", price: "$3.50")
+    snack1 = Snack.create(name: "Pop Rocks", price: "$1.50")
+    owner = Owner.create(name: "Sam's Snacks", id: 1)
+    machine = Machine.create(location: "Don's Mixed Drinks")
+
+    visit snack_path(snack)
+
+    expect(page).to have_content(machine.location)
   end
 end
